5b31371e11928615f5c5ed73c7937795694d07c2
Alexa Sanchez <alexsc167@gmail.com>
2019-11-13 19:24:21 +0000

    functionalities

--- server.js
+++ server.js
@@ -40,10 +40,38 @@ app.post('/users', (req, res)=>{
 });
 
 app.post('/usersP', (req, res) => {
-    const newUser = req.body;
-    newUser.id = Math.random();
-    users.push(newUser);
-    res.json(newUser);
-  });
+    if(Object.keys(req.body).length === 0){
+        res.status(400).send('debe pasar algo por el body')
+    }
+    else {
+        const newUser = req.body
+        newUser.id = Math.random();
+        users.push(newUser);
+        res.json(newUser);
+    }
+});
+
+
+const mandatoryField = (mandatory, bodyJSON) => {
+
+    if (bodyJSON) {
+        return false;
+    }
+
+    const stringJSON = JSON.stringify(bodyJSON)
+    let validationsCount = 0;
+
+    mandatory.forEach(element => {
+        if (stringJSON.includes(element)) {
+            validationsCount += 1;
+        }
+    });
+
+    if (validationsCount == mandatoryField.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 app.listen(3000, ()=> console.log('Ready on port 3000!'));
